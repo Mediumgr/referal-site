b8aeed1325abe1a2d6afb141c68b8923cff0119b
Руслан <Mediumgr@yandex.ru>
2023-08-31 12:23:21 +0300

    Initial

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from '/node_modules/vue/dist/vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
